c8257a9e7cd42407496bd2da93c76492880b0fad
郭永杰 <gyj0418@outlook.com>
2017-07-25 13:43:39 +0800

    整理，发布

--- src/routes/i18n.ts
+++ src/routes/i18n.ts
@@ -7,7 +7,7 @@ import I18n from '../domain/I18n/I18nModel'
 
 export default (router: Router) => {
   router
-    //同步数据库
+    // 同步数据库
     .get('/i18n-sync', async function(ctx: IRouterContext) {
       const _apiPath = path.resolve(__dirname, '../../data/i18n.json')
       const apiObject = JSON.parse(fs.readFileSync(_apiPath, 'utf-8'))
@@ -55,25 +55,25 @@ export default (router: Router) => {
     /**
      * 导入数据
      */
-    .put('/i18n/import/:data', async function(ctx: IRouterContext){
+    .put('/i18n/import/:data', async function(ctx: IRouterContext) {
       try {
         const params = JSON.parse(ctx.params.data)
         const project = params.projectId
         const lang = params.lang
         const langData = JSON.parse(params.data)
 
-        //解析、插入数据
+        // 解析、插入数据
         const keys = Object.keys(langData)
-        keys.map(async (key)=>{
+        keys.map(async (key) => {
           const result = (await I18n.find({ project, key, lang }))
-          if(result.length>0){
+          if (result.length > 0) {
             I18n.update({ project, key, lang }, { value: langData[key] })
-          }else{
+          } else {
             I18n.create({ project, key, lang, value: langData[key] })
           }
         })
 
-        ctx.body = { code:1, msg: 'success' }
+        ctx.body = { code: 1, msg: 'success' }
       } catch (e) {
         ctx.body = { code: -1, data: e }
       }
@@ -94,16 +94,16 @@ export default (router: Router) => {
         const newData = JSON.parse(ctx.params.data)
 
         const langs = Object.keys(newData)
-        langs.map(async (lang)=>{
+        langs.map(async (lang) => {
           const result = (await I18n.find({ project, key, lang }))
-          if(result.length>0){
+          if (result.length > 0) {
             await I18n.update({ project, key, lang }, { value: newData[lang] })
-          }else{
+          } else {
             await I18n.create({ project, key, lang, value: newData[lang] })
           }
         })
 
-        ctx.body = { code:1, msg: 'success' }
+        ctx.body = { code: 1, msg: 'success' }
       } catch (e) {
         ctx.body = { code: -1, data: e }
       }
