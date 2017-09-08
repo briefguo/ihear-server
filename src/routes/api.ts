9365499905b599cb8b29602a2115bc48160f52c3
郭永杰 <gyj0418@outlook.com>
2017-09-08 15:51:15 +0800

    整理保存

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -73,23 +73,35 @@ export default (router: Router) => {
         ctx.body = { code: -1, data: 'file_not_found', err: e }
       }
     })
+    .get('/delete/:id', async function (ctx: IRouterContext) {
+      try {
+        const _id = ctx.params.id
+        ctx.body = await Api.remove({ _id })
+      } catch (e) {
+        ctx.body = { code: -1, data: 'file_not_found', err: e }
+      }
+    })
 
     // 新建或更新一条API
     .put('/api/:name/:data', async function (ctx: IRouterContext) {
       try {
         const name = ctx.params.name
         const newData = JSON.parse(ctx.params.data)
-        const project = newData.projectId
-
-        const results = (await Api.find({ name })).filter((item: any) => item.project === project)
-
+        const project = newData.project
+        const results = await Api.find({ name, project })
         if (results.length > 0) {
-          ctx.body = await Api.update({ name }, { ...newData, name })
+          ctx.body = {
+            results,
+            data: await Api.update({ name }, { ...newData, name }),
+          }
         } else {
-          ctx.body = await Api.create({
-            ...newData,
-            name: ctx.params.name
-          })
+          ctx.body = {
+            results,
+            data: await Api.create({
+              ...newData,
+              name: ctx.params.name
+            }),
+          }
         }
       } catch (err) {
         ctx.body = { code: -1, data: err }
