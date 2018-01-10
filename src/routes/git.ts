641d85caa205237239732a3abbd040d8c14ece43
郭永杰 <gyj0418@outlook.com>
2018-01-10 18:51:10 +0800

    整理保存

--- src/routes/git.ts
+++ src/routes/git.ts
@@ -2,18 +2,27 @@
 import Router, { IRouterContext } from 'koa-router';
 var exec = require('child_process').exec;
 
-export default (http: Router, socketIO: SocketIO.Server) => {
+const execPromise = async (cmdStr) => {
+  return new Promise((resolve) => {
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        resolve(stderr)
+      } else {
+        resolve(stdout)
+      }
+    })
+  })
+}
+
+export default (http: Router) => {
   http.get('/git', async (ctx: IRouterContext) => {
     try {
-      console.log(ctx.query);
-      // const repoDir = path.resolve('./');
-      const { repoDir, branchMapJSON } = ctx.query;
-      const cmdStr = `cd ${repoDir} && git pull`;
-	exec(cmdStr, function(err,stdout,stderr){
-	});
-      ctx.body = { code: 1 };
+      const { repoDir } = ctx.query
+      const cmdStr = `cd ${repoDir} && git pull`
+      const result = await execPromise(cmdStr)
+      ctx.body = result
     } catch (error) {
-	console.log(error)
+      // console.log(error)
       ctx.body = { code: -999, error };
     }
   });
